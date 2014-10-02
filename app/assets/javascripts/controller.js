bd3db039e5596ed8995072df937b46c8aff48b55
songyiyang <songyiyang2012@gmail.com>
2014-10-02 14:43:04 -0400

    add video

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   });
   $('body').prepend('<div class="video-background"></div>');
   $('.video-background').videobackground({
-    videoSource: [['http://pdl.vimeocdn.com/94264/560/243223128.mp4?token2=1398291499_7ce3d2d808c1f90bf36774c54e59f679&aksessionid=1c8467a2d3f29535', 'video/mp4']],
+    videoSource: [['https://s3-us-west-2.amazonaws.com/yiyang/backvideo-HD.mp4', 'video/mp4']],
     controlPosition: '#main',
     loadedCallback: function() {
       $(this).videobackground('mute');
